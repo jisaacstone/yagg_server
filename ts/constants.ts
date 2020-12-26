59516bc1f0566864487ebacd41955f33d9a339aa
jisaacstone <jisaacstone@gmail.com>
2020-12-25 21:12:09 -0800

    better symbol for move

--- ts/constants.ts
+++ ts/constants.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
-export const SKULL = String.fromCodePoint(0x1F480);
+export const SKULL = String.fromCodePoint(0x1F480) + String.fromCodePoint(0xFE0E);
 // @ts-ignore
-export const MOVE = String.fromCodePoint(0x2608);
+export const MOVE = String.fromCodePoint(0x27BD) + String.fromCodePoint(0xFE0E);
 // @ts-ignore
-export const ATTACK = String.fromCodePoint(0x2694);
+export const ATTACK = String.fromCodePoint(0x2694) + String.fromCodePoint(0xFE0E);
